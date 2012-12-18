f4c3341662dc51ce5478d0314d4c5988396c05de
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2012-12-17 23:09:04 -0500

    + stitch (doesn't work)

--- fitnesse.js
+++ fitnesse.js
@@ -1,13 +1,20 @@
 var path = require('path');
 var express = require('express');
+var stitch  = require('stitch');
 var _ = require('underscore');
 
-var config = {};
+var _config = {};
+_.defaults(_config, { port: 80 } );
+
+var _package = stitch.createPackage({
+  paths: [__dirname + '/src']
+});
+
 var app = express();
-_.defaults(config, { port: 80 } );
 app.set('views', __dirname + '/views');
+app.get('/application.js', _package.createServer());
 app.get('/', function(req, res){res.render('index.jade');});
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(config.port);
-console.log('Listening on port', config.port);
+app.listen(_config.port);
+console.log('Listening on port', _config.port);
---
+++ src/app.js
@@ -0,0 +1,17 @@
+var App = Ember.Application.create();
+
+App.ApplicationController = Ember.Controller.extend();
+App.ApplicationView = Ember.View.extend({
+  templateName: 'application',
+  name: 'velaskec'
+});
+
+App.Router = Ember.Router.extend({
+  root: Ember.Route.extend({
+    index: Ember.Route.extend({
+      route: '/'
+    })
+  })
+})
+
+App.initialize();
